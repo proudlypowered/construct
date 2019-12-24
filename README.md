c4abc27ecae392f52504765bfae31d8414d4b080
Mark Hesketh <contact@markhesketh.co.uk>
2019-12-24 21:16:47 +0000

    Initial commit 🚀

---
+++ README.md
@@ -0,0 +1,13 @@
+# Quintessential
+
+A collection of quintessential options and best practices to improve any WordPress website.
+
+This project is currently a work in progress.
+
+## Requirements
+
+* PHP >=5.4
+
+## License
+
+This role is open-sourced software licensed under the [GPLv3 License](https://www.gnu.org/licenses/gpl-3.0.html).
---
+++ quintessential.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:     Quintessential
+ * Plugin URI:      https://www.proudlypowered.com/plugins/quintessential
+ * Description:     A collection of quintessential options and best practices to improve any WordPress website.
+ * Author:          Proudly Powered
+ * Author URI:      https://www.proudlypowered.com
+ * Text Domain:     construct
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ * License:         GPLv3
+ * License URI:     https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package         Construct
+ */
+
+defined( 'ABSPATH' ) || exit;
